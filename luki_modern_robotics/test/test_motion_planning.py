b942d4b0446ed6533338aadbf99457eeac3b26a5
Lukas Woodtli <woodtli.lukas@gmail.com>
2023-06-20 22:10:05 +0200

    Make A* algorithm more object oriented

--- luki_modern_robotics/test/test_motion_planning.py
+++ luki_modern_robotics/test/test_motion_planning.py
@@ -31,163 +31,175 @@ class OpenList:
             return "oo"
         return self.dict[value]
 
-open_list = OpenList()
-closed_list = set()
+class AStar:
+
+    def __init__(self, start, end, cost_list, heuristic_cost_to_go):
+        self.open_list = OpenList()
+        self.closed_list = set()
+        self.cost = self.init_cost(cost_list)
+        self.heuristic_cost_to_go = self.init_heuristic_cost_to_go(heuristic_cost_to_go)
+        self.start = start
+        self.end = end
+        # minimum cost so far for node (from start)
+        self.past_cost = defaultdict(lambda: 99)
+        self.parent = {}
+        self.init_a_star()
+
+    def init_a_star(self):
+        self.open_list.insert(self.start, self.heuristic_cost_to_go[self.start])
+        self.past_cost[self.start] = 0
+
+    @staticmethod
+    def init_cost(cost_list):
+        # costs per edge
+        res = defaultdict(lambda: defaultdict(lambda: None))
+        for cost in cost_list:
+            start, end, cst = cost
+            res[start][end] = cst
+        # not a directed graph so cost[i][j] == cost[j][i]
+        for i in range(1, 6):
+            for j in range(1, 6):
+                if i != j:
+                    if res[i][j]:
+                        res[j][i] = res[i][j]
+        return res
+
+    @staticmethod
+    def init_heuristic_cost_to_go(htg):
+        heuristic_cost_to_go = {}
+        for i in htg:
+            node, cost = i
+            heuristic_cost_to_go[node] = cost
+        return heuristic_cost_to_go
+
+    def get_neighbors(self, node):
+        result = set()
+        for k0, v0 in self.cost.items():
+            for k1, v1 in v0.items():
+                if v0 and v1 and node in (k0, k1):
+                    result.add(k0)
+                    result.add(k1)
+        return result
+
+    def get_neighbors_not_in_closed(self, node):
+        nbrs = self.get_neighbors(node)
+        return [n for n in nbrs if n not in self.closed_list]
+
+    def a_star(self):
+        while not self.open_list.empty():
+            current = self.open_list.get_first()
+            log(f"\ncurrent: {current}")
+            if current == self.end:
+                return self.get_path(self.end)
+            self.closed_list.add(current)
+            for nbr in self.get_neighbors_not_in_closed(current):
+                self.process_neighbor(current, nbr)
+        return None
+
+    def process_neighbor(self, current, nbr):
+        log(f"\nnbr: {nbr}")
+        tentative_past_cost = self.past_cost[current] + self.cost[current][nbr]
+        if tentative_past_cost < self.past_cost[nbr]:
+            self.update_best_path(current, nbr, tentative_past_cost)
+        self.print_all()
 
 
-# costs per edge
-cost = defaultdict(lambda: defaultdict(lambda: None))
+    def update_best_path(self, current, nbr, tentative_past_cost):
+        self.past_cost[nbr] = tentative_past_cost
+        self.parent[nbr] = current
+        est_total_cost = self.past_cost[nbr] + self.heuristic_cost_to_go[nbr]
+        self.open_list.insert(nbr, est_total_cost)
 
-cost[1][3] = 18
-cost[1][4] = 12
-cost[1][5] = 30
-cost[2][3] = 27
-cost[2][6] = 10
-cost[3][6] = 15
-cost[4][5] = 8
-cost[4][6] = 20
-cost[5][6] = 10
+    def print_all(self):
+        s = self.collect_information()
+        log("\n" + s)
+        log("="*40)
 
-# not a directed graph so cost[i][j] == cost[j][i]
-for i in range(1, 6):
-    for j in range(1, 6):
-        if i != j:
-            if cost[i][j]:
-                cost[j][i] = cost[i][j]
+    @staticmethod
+    def print_entry_for_nodes(row_title, list_entries):
+        ret = f"\n{f'{row_title}:':<15}"
+        for e in range(1, 7):
+            ret += f"{list_entries[e]:>3} | "
+        return ret
 
-# start node
-start = 1
-# end node
-end = 6
+    def collect_information(self):
+        s = self.print_entry_for_nodes("Past cost", self.past_cost)
 
-def get_neighbors(node):
-    result = set()
-    for k0, v0 in cost.items():
-        for k1, v1 in v0.items():
-            if v0 and v1 and node in (k0, k1):
-                result.add(k0)
-                result.add(k1)
-    return result
+        s += self.print_entry_for_nodes("optimist ctg", self.heuristic_cost_to_go)
 
-def get_neighbors_not_in_closed(node):
-    nbrs = get_neighbors(node)
-    return [n for n in nbrs if n not in closed_list]
+        lst = {i: self.open_list.get_est_total_cost(i) for i in range(1, 7)}
+        s += self.print_entry_for_nodes("est tot cost", lst)
 
+        lst1 = {i1: self.parent.get(i1, "-") for i1 in range(1, 7)}
+        s += self.print_entry_for_nodes('parent node', lst1)
 
-# minimum cost so far for node (from start)
-past_cost = defaultdict(lambda: 99)
+        s += self.print_open()
+        s += self.print_closed()
+        return s
 
-heuristic_cost_to_go = {}
-heuristic_cost_to_go[1] = 20
-for i in range(2, 6):
-    heuristic_cost_to_go[i] = 10
-heuristic_cost_to_go[6] = 0
+    def get_path(self, item):
+        path = []
+        nxt = item
+        while nxt != self.start:
+            path = [nxt] + path
+            nxt = self.parent[nxt]
+        path = [self.start] + path
+        return path
 
-parent = {}
+    def print_open(self):
+        ret = "\nOPEN\n"
+        lst = list(self.open_list.dict.items())
+        lst.sort(key=lambda x: x[1])
+        for l in lst:
+            ret += f"{l[0]} ({l[1]}), "
+        ret += '\n'
+        return ret
 
-
-def a_star():
-    init_a_star(start)
-    while not open_list.empty():
-        current = open_list.get_first()
-        log(f"\ncurrent: {current}")
-        if current == end:
-            return end
-        closed_list.add(current)
-        for nbr in get_neighbors_not_in_closed(current):
-            process_neighbor(current, nbr)
-    return None
+    def print_closed(self):
+        ret = "CLOSED\n"
+        lst = list(self.closed_list)
+        lst.sort()
+        for l in lst:
+            ret += f"{l}, "
+        ret += "\n\n"
+        return ret
 
 
-def process_neighbor(current, nbr):
-    log(f"\nnbr: {nbr}")
-    tentative_past_cost = past_cost[current] + cost[current][nbr]
-    if tentative_past_cost < past_cost[nbr]:
-        update_best_path(current, nbr, tentative_past_cost)
-    print_all()
+test_cost_list = [
+    [1, 3, 18],
+    [1, 4, 12],
+    [1, 5, 30],
+    [2, 3, 27],
+    [2, 6, 10],
+    [3, 6, 15],
+    [4, 5, 8],
+    [4, 6, 20],
+    [5, 6, 10]
+]
 
-
-def update_best_path(current, nbr, tentative_past_cost):
-    past_cost[nbr] = tentative_past_cost
-    parent[nbr] = current
-    est_total_cost = past_cost[nbr] + heuristic_cost_to_go[nbr]
-    open_list.insert(nbr, est_total_cost)
-
-
-def init_a_star(start):
-    open_list.insert(start, heuristic_cost_to_go[start])
-    past_cost[start] = 0
-
-
-def print_all():
-    s = collect_information()
-    log("\n" + s)
-    log("="*40)
-
-def print_entry_for_nodes(row_title, list_entries):
-    ret = f"\n{f'{row_title}:':<15}"
-    for e in range(1, 7):
-        ret += f"{list_entries[e]:>3} | "
-    return ret
-
-def collect_information():
-    s = print_entry_for_nodes("Past cost", past_cost)
-
-    s += print_entry_for_nodes("optimist ctg", heuristic_cost_to_go)
-
-    lst = {i: open_list.get_est_total_cost(i) for i in range(1, 7)}
-    s += print_entry_for_nodes("est tot cost", lst)
-
-    lst1 = {i1: parent.get(i1, "-") for i1 in range(1, 7)}
-    s += print_entry_for_nodes('parent node', lst1)
-
-    s += print_open()
-    s += print_closed()
-    return s
-
-
-def get_path(item):
-    path = []
-    nxt = item
-    while nxt != start:
-        path = [nxt] + path
-        nxt = parent[nxt]
-    path = [start] + path
-    return path
-
-
-def print_open():
-    ret = "\nOPEN\n"
-    lst = list(open_list.dict.items())
-    lst.sort(key=lambda x: x[1])
-    for l in lst:
-        ret += f"{l[0]} ({l[1]}), "
-    ret += '\n'
-    return ret
-
-def print_closed():
-    ret = "CLOSED\n"
-    lst = list(closed_list)
-    lst.sort()
-    for l in lst:
-        ret += f"{l}, "
-    ret += "\n\n"
-    return ret
-
+test_heuristic_cost_to_go = [
+    [1, 20],
+    [2, 10],
+    [3, 10],
+    [4, 10],
+    [5, 10],
+    [6, 0],
+]
 
 def test_cost():
-    assert not cost[1][6]
-    assert not cost[4][2]
-    assert cost[4][1] == 12
-    assert cost[3][6] == 15
-    assert cost[3][2] == 27
+    a_star = AStar(1, 6, test_cost_list, test_heuristic_cost_to_go)
+    assert not a_star.cost[1][6]
+    assert not a_star.cost[4][2]
+    assert a_star.cost[4][1] == 12
+    assert a_star.cost[3][6] == 15
+    assert a_star.cost[3][2] == 27
 
 def test_init_a_star():
-    init_a_star(start)
-    assert len(open_list.dict) == 1
-    assert open_list.dict[1] == 20
-    assert past_cost[1] == 0
-    assert len(closed_list) == 0
+    a_star = AStar(1, 6, test_cost_list, test_heuristic_cost_to_go)
+    assert len(a_star.open_list.dict) == 1
+    assert a_star.open_list.dict[1] == 20
+    assert a_star.past_cost[1] == 0
+    assert len(a_star.closed_list) == 0
 
 
 
@@ -296,7 +308,7 @@ CLOSED
 ========================================
 current: 6"""
 
-    parent = a_star()
-    path = get_path(parent)
+    a_star = AStar(1, 6, test_cost_list, test_heuristic_cost_to_go)
+    path = a_star.a_star()
     assert expected == log_str
     assert path == [1, 4, 5, 6]
